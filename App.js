26eb04f9e4792a46079ab23e2e4debc195c0c6d4
shoryataneja <shoryataneja5@gmail.com>
2025-10-01 17:57:37 +0530

    adding the camera to my application

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [permission, requestPermission] = useCameraPermissions();
+  
+  const askPermission = async () => {
+        try{
+          await requestPermission();
+        }catch(err){
+          console.log(err)
+        }
+      }
+    
+  useEffect(()=>{
+    askPermission();
+  },[])
+
+
+  console.log(permission);
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <CameraView style = {styles.camera}/>
+      <Text style = {styles.text}>How much pink is too much pink ?!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +35,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
   },
-});
+  message: {
+    textAlign: "center",
+    paddingBottom: 10,
+  },
+  camera: {
+    height: 300,
+    width: 300,
+  },
+  buttonContainer: {
+    position: "absolute",
+    bottom: 64,
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    width: "100%",
+    paddingHorizontal: 64,
+  },
+  button: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#2196F3",
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    borderRadius: 6,
+    marginTop: 12,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+  image: {
+    height: 300,
+    width: 300,
+    paddingTop: 10,
+  },
+})
